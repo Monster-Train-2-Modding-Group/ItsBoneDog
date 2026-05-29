e98799343d0e3327fbb787bd3128323b72cad656
Brandon <tricksterguy87@gmail.com>
2026-05-29 14:14:03 -0700

    Files

---
+++ ItsBoneDog.Plugin/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TrainworksReloaded.Core;
+using TrainworksReloaded.Core.Extensions;
+
+namespace ItsBoneDog.Plugin
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger = new(MyPluginInfo.PLUGIN_GUID);
+        
+        public void Awake()
+        {
+            Logger = base.Logger;
+
+            var builder = Railhead.GetBuilder();
+            builder.Configure(
+                MyPluginInfo.PLUGIN_GUID,
+                c =>
+                {
+                    c.AddMergedJsonFile(
+                        "json/plugin.json"
+                    );
+                }
+            );
+
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ItsBoneDog
+
+Visual edit of the BoneDog card with all of the hints for the guess-that-card competition
